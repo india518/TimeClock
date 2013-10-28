c216f66679cbefb1202af8757cbf8c1662677b5f
India Meisner <india.rae@gmail.com>
2013-10-27 22:50:20 -0700

    Add methods to Users and Shifts controller, including strong parameters. Remove public/index.html file.

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,2 +1,30 @@
 class ShiftsController < ApplicationController
+
+	def new
+		@shift = Shift.new
+	end
+
+	def create
+		@shift = User.find(params[:user_id]).shifts.new(shift_params)
+		if @shift.save
+			redirect_to summary_path,
+				:flash => { :notice => '#{@shift.user.name} has clocked in!' }
+	end
+
+	def edit
+		@shift = Shift.find(params[:id])
+	end
+
+	def update
+		@shift = Shift.find(params[:id])
+		@shift.update_attributes(:updated_at => Time.now)
+	end
+
+	# def destroy
+	# end
+
+	private
+	def shift_params
+  	params.require(:shift).permit(:note)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.all
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name)
   end
 end
