1de4045cde3935663540b744652f0838cf6f24ea
India Meisner <india.rae@gmail.com>
2013-10-27 22:37:51 -0700

    Add is_in_progress?/is_over?/time_clocked methods to Shift model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -14,4 +14,24 @@ class Shift < ActiveRecord::Base
   def clock_out_time
   	self.updated_at
   end
+
+  def is_in_progress?
+  	#The way Rails is creating models, the 'updated_at' is always set to 
+  	#'created_at' when a new model is created. We have a two options:
+  	#Which one is better?
+  	self.created_at === self.updated_at
+  	#(self.created_at - self.updated_at) === 0
+  end
+
+  #May not need; but it may be easier to read code calling this method?
+  def is_over?
+  	!self.is_in_progress?
+  end
+
+  def time_clocked
+  	return nil if self.is_in_progress?
+  	(self.updated_at - self.created_at)/3600 #convert from seconds to hours
+  	#Note to self, for table in view:
+  	# sprintf "%.2f", 1.0393477 #=> "1.04"
+  end
 end
