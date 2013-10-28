f4ea255b600c64015bb3f21c1e4162959befc2d9
India Meisner <india.rae@gmail.com>
2013-10-27 22:25:37 -0700

    Fix typos in routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 TimeClock::Application.routes.draw do
-  root :to => 'users#index'
-  get '/summary', :as => 'users#index'
+  get '/summary', :to => 'users#index'
   
-  get '/clock_in', :as => 'shifts#new'
-  post '/clock_in', :as => 'shifts#create'
-  get '/clock_out', :as => 'shifts#edit'
-  post '/clock_out', :as => 'shifts#update'
-  resources :shifts => :except [index, show]
-  #resources :shifts => :except [index, show, destroy]
+  get '/clock_in', :to => 'shifts#new'
+  post '/clock_in', :to => 'shifts#create'
+  get '/clock_out', :to => 'shifts#edit'
+  post '/clock_out', :to => 'shifts#update'
+  resources :shifts, :except => [:index, :show]
+  #resources :shifts, :except => [:index, :show, :destroy]
+
+  root :to => 'users#index'
 end
