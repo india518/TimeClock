038f423d90489c549e3cd2344f5b026f0bf90a80
India Meisner <india.rae@gmail.com>
2013-10-28 18:08:26 -0700

    Populated seeds.rb in case something happens to users table, I don't have to retype it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  
+  User.create(first_name: "Muffin",		last_name: "Kitty")
+  User.create(first_name: "Biscuit",	last_name: "Kitty")
+  User.create(first_name: "Space",		last_name: "Cowboy")
+  User.create(first_name: "Test",			last_name: "Testerson")
+  User.create(first_name: "Thomas",		last_name: "Edison")
+  User.create(first_name: "Jay",			last_name: "Gatsby")
+  User.create(first_name: "Daisy",		last_name: "Buchanan")
+  User.create(first_name: "Nick",			last_name: "Carraway")
+  User.create(first_name: "Tom",			last_name: "Buchanan")
+  User.create(first_name: "Jordan", 	last_name: "Baker")
+  
+end
